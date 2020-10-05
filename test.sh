b723e58d4d3f02a9c914014c90fbc530135e9da4
derrick921213 <dlin12457@gmail.com>
2020-10-05 12:44:25 +0800

    test

--- test.sh
+++ test.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 
-echo "=== 將目前執行 process 的 PID 依照數字大小排序，取出前 10 名 === "
-ps | awk '{print $1}' | sort -rn | head -10
+pymod=flask,
 
-result=`expr 10 + 2`
+warn()
+{
+  echo "Warning: $*"
+}
 
-# 12
-echo "Result: $result"
+found()
+{
+  echo "$* found"
+}
+
+if python -c "import $pymod" >/dev/null 2>&1
+then
+    found "$pymod"
+else
+    warn "$pymod: NOT FOUND"
+fi
