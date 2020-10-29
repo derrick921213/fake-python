f4349035c1aafcfefd77dea347f0e5c661c43f21
derrick921213 <dlin12457@gmail.com>
2020-10-29 16:33:21 +0800

    change

--- setup.py
+++ setup.py
@@ -1,5 +1,4 @@
 import os
 print('--------Download install file--------')
-os.system("git svn clone https://github.com/derrick921213/fake-python/trunk/dist;cd dist;svn checkout https://github.com/derrick921213/fake-python/trunk/commands")
+os.system("svn checkout https://github.com/derrick921213/fake-python/trunk/dist;cd dist;svn checkout https://github.com/derrick921213/fake-python/trunk/commands")
 print('--------Download fisished--------')
-#checkout
