f828303b8706815e8ec33220f8fe650ea991e660
derrick921213 <dlin12457@gmail.com>
2020-10-29 20:47:11 +0800

    add

--- setup.py
+++ setup.py
@@ -1,4 +1,12 @@
-import os
-print('--------Download install file--------')
-os.system("svn checkout https://github.com/derrick921213/fake-python/trunk/dist;cd dist;svn checkout https://github.com/derrick921213/fake-python/trunk/commands")
-print('--------Download fisished--------')
+import os,platform as p
+system = p.system()
+if (system == 'Darwin'):
+    print('MacOS')
+elif (system == 'Linux'):
+    print('Linux')
+else:
+    print('This app only support Mac and Linux')        
+
+#print('--------Download install file--------')
+#os.system("svn checkout https://github.com/derrick921213/fake-python/trunk/dist;cd dist;svn checkout https://github.com/derrick921213/fake-python/trunk/commands")
+#print('--------Download fisished--------')
