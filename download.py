67edd86674234ac908b3f5dc6e60157bc2e9c29a
derrick921213 <dlin12457@gmail.com>
2020-10-29 21:29:37 +0800

    add

--- download.py
+++ download.py
@@ -3,10 +3,12 @@ system = p.system()
 if (system == 'Darwin'):
     print('--------Download Mac install file--------')
     os.system("svn checkout https://github.com/derrick921213/fake-python/trunk/setdone/mac")
+    print('In mac folder use ./Mac-setup')
     print('--------Download fisished--------')
 elif (system == 'Linux'):
     print('--------Download Linux install file--------')
     os.system("svn checkout https://github.com/derrick921213/fake-python/trunk/setdone/linux")
+    print('In linux folder use ./Linux-setup')
     print('--------Download fisished--------')
 else:
     print('This app only support Mac and Linux')
