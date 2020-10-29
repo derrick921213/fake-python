5f7e3ef8b1b2e5d90ed7f2a73d0e3da3ebefe177
derrick921213 <dlin12457@gmail.com>
2020-10-29 21:33:44 +0800

    add

--- setdone/setup.py
+++ setdone/setup.py
@@ -2,11 +2,11 @@ import os,platform as p
 system = p.system()
 if (system == 'Darwin'):
     print('--------Download Mac install file--------')
-    os.system("svn checkout https://github.com/derrick921213/fake-python/trunk/done/mac;cd dist;svn checkout https://github.com/derrick921213/fake-python/trunk/commands")
+    os.system("svn checkout https://github.com/derrick921213/fake-python/trunk/done/mac-main;cd dist;svn checkout https://github.com/derrick921213/fake-python/trunk/commands")
     print('--------Download fisished--------')
 elif (system == 'Linux'):
     print('--------Download Linux install file--------')
-    os.system("svn checkout https://github.com/derrick921213/fake-python/trunk/done/linux;cd dist;svn checkout https://github.com/derrick921213/fake-python/trunk/commands")
+    os.system("svn checkout https://github.com/derrick921213/fake-python/trunk/done/linux-main;cd dist;svn checkout https://github.com/derrick921213/fake-python/trunk/commands")
     print('--------Download fisished--------')
 else:
     print('This app only support Mac and Linux')        
