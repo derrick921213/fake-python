87cfaac9284d3e86a0843a7efc2ea5f5b320dad7
derrick921213 <dlin12457@gmail.com>
2020-10-30 10:10:01 +0800

    add

--- setdone/setup.py
+++ setdone/setup.py
@@ -2,7 +2,7 @@ import os,platform as p
 system = p.system()
 if (system == 'Darwin'):
     print('--------Download Mac install file--------')
-    os.system("mkdir dist;cd dist;svn checkout https://github.com/derrick921213/fake-python/trunk/done/mac-main;svn checkout https://github.com/derrick921213/fake-python/trunk/commands;mv commands mac-main")
+    os.system("git checkout https://github.com/derrick921213/fake-python/trunk/done/mac-main;svn checkout https://github.com/derrick921213/fake-python/trunk/commands")
     print('--------Download fisished--------')
 elif (system == 'Linux'):
     print('--------Download Linux install file--------')
