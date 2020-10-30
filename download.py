afbf87bfc4cdec5d49f6c74b0a94dfddced05299
derrick921213 <dlin12457@gmail.com>
2020-10-30 23:26:10 +0800

    add

--- download.py
+++ download.py
@@ -6,11 +6,12 @@ if (system == 'Darwin'):
     os.system("svn checkout https://github.com/derrick921213/fake-python/trunk/setdone/mac")
     print('In mac folder use ./Mac-setup')
     print('--------Download fisished--------')
-    os.system("cd mac;./Mac-setup;cd mac-main;./Mac-main --install;mv Mac-main ~/Documents/pycontroler")
+    os.system("cd mac;./Mac-setup;cd mac-main;./Mac-main --install;cd ..;mv mac-main ~/Documents/pycontroler")
 elif (system == 'Linux'):
     print('--------Download Linux install file--------')
     os.system("svn checkout https://github.com/derrick921213/fake-python/trunk/setdone/linux")
     print('In linux folder use ./Linux-setup')
     print('--------Download fisished--------')
+    os.system("cd linux;./Linux-setup;cd linux-main;./Linux-main --install;cd ..;mv linux-main ~/Documents/pycontroler")
 else:
     print('This app only support Mac and Linux')
